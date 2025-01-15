fec3d543661481825016c30548c9d22b7087d6f6
Mohammed Molla <mjmolla@gmail.com>
2025-01-14 20:22:45 -0500

    maven stracture

---
+++ src/main/java/dataType/NumericData.java
@@ -0,0 +1,10 @@
+package dataType;
+
+public class NumericData {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.print("hello");
+	}
+
+}
---
+++ src/test/java/facebookWebDriver/HowToOpenFacebook.java
@@ -0,0 +1,5 @@
+package facebookWebDriver;
+
+public class HowToOpenFacebook {
+
+}
